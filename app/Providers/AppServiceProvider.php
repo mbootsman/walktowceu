56726b33dcfedc2312ad5fdbbf6edcc8b1b36c55
Marcel Bootsman <marcel@nostromo.nl>
2023-06-15 19:47:08 +0200

    Upgraded statamic/cms to 4.6.0

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,55 +4,25 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Statamic\Statamic;
-use Statamic\StaticSite\SSG;
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
-     *
-     * @return void
      */
-    public function register()
+    public function register(): void
     {
         //
     }
 
     /**
      * Bootstrap any application services.
-     *
-     * @return void
      */
-    public function boot()
+    public function boot(): void
     {
-        // Statamic::script('app', 'cp');
-        // Statamic::style('app', 'cp');
-
-        SSG::after(function () {
-            $from = public_path('img');
-            $to = config('statamic.ssg.destination').'/img';
-            app('files')->copyDirectory($from, $to);
-        });
-
-        if ($this->app->runningInConsole()) {
-            $this->bootSsg();
-        }
-
-    }
-
-    private function bootSsg()
-    {
-        $this->app->extend(LengthAwarePaginator::class, function ($paginator) {
-            $options = $paginator->getOptions();
-            $options['path'] = preg_replace('/\/page\/\d+$/', '', $options['path']);
-
-            return $this->app->makeWith(UrlPaginator::class, [
-                'items' => $paginator->getCollection(),
-                'total' => $paginator->total(),
-                'perPage' => $paginator->perPage(),
-                'currentPage' => $paginator->currentPage(),
-                'options' => $options,
-            ]);
-        });
+        // Statamic::vite('app', [
+        //     'resources/js/cp.js',
+        //     'resources/css/cp.css',
+        // ]);
     }
 }
